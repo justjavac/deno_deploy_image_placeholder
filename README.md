d9d6b72b3fb3bd28e15f1eaacedc843f534a05af
迷渡 <justjavac@gmail.com>
2022-04-27 16:40:30 +0800

    SVG 图片占位图
    Signed-off-by: 迷渡 <justjavac@gmail.com>

---
+++ README.md
@@ -0,0 +1,11 @@
+# deno_deploy_image_placeholder
+
+SVG 图片占位图
+
+![](https://imgph.deno.dev)
+
+**使用方式**
+
+```
+https://imgph.deno.dev?w=[width]&h=[height]?t=[文字]&bg=[背景颜色]&fg=[文字颜色]
+```
---
+++ main.ts
@@ -0,0 +1,20 @@
+import { serve } from "https://deno.land/std@0.136.0/http/server.ts";
+
+console.log("Listening on http://localhost:8000");
+
+serve((request) => {
+  const searchParams = new URL(request.url).searchParams;
+  const w = parseInt(searchParams.get("w") ?? "400");
+  const h = parseInt(searchParams.get("h") ?? "300");
+  const fg = searchParams.get("fg") ?? "FFF";
+  const bg = searchParams.get("bg") ?? "CCC";
+  const t = searchParams.get("t") ?? `${w}x${h}`;
+  const fontSize = h / 4;
+  const svg = `<svg xmlns='http://www.w3.org/2000/svg' width='${w}' height='${h}'>
+  <rect x='0' y='0' width='${w}' height='${h}' fill='#${bg}'/>
+  <text x='50%' y='50%' style='dominant-baseline:middle;text-anchor:middle;font-size:${fontSize}px' fill='#${fg}'>${t}</text>
+</svg>`;
+  return new Response(svg, {
+    headers: { "content-type": "image/svg+xml" },
+  });
+});
