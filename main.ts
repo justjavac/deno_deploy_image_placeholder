63d34efcf1a53dea2e51c04d93deb97a8abdf766
迷渡 <justjavac@gmail.com>
2022-04-27 16:48:18 +0800

    fix fmt
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- main.ts
+++ main.ts
@@ -10,7 +10,8 @@ serve((request) => {
   const bg = searchParams.get("bg") ?? "CCC";
   const t = searchParams.get("t") ?? `${w}x${h}`;
   const fontSize = h / 4;
-  const svg = `<svg xmlns='http://www.w3.org/2000/svg' width='${w}' height='${h}'>
+  const svg =
+    `<svg xmlns='http://www.w3.org/2000/svg' width='${w}' height='${h}'>
   <rect x='0' y='0' width='${w}' height='${h}' fill='#${bg}'/>
   <text x='50%' y='50%' style='dominant-baseline:middle;text-anchor:middle;font-size:${fontSize}px' fill='#${fg}'>${t}</text>
 </svg>`;
