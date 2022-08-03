988f95527b094bded07f74f65f43ba02c62d0a42
迷渡 <justjavac@gmail.com>
2022-08-03 21:40:50 +0800

    upgrade std@0.150.0 (#6)

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.136.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.150.0/http/server.ts";
 
 console.log("Listening on http://localhost:8000");
 
